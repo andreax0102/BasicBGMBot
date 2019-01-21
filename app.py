fe774304f3118c6273d0b72509ce0147760ccff3
andreax0102 <andreax0102@gmail.com>
2019-01-20 18:50:03 -0800

    only echo and print output

--- app.py
+++ app.py
@@ -21,6 +21,7 @@ def receive_message():
     else:
         # get whatever message a user sent the bot
        output = request.get_json()
+       print("output: ", output)
        for event in output['entry']:
           messaging = event['messaging']
           for message in messaging:
@@ -28,7 +29,7 @@ def receive_message():
                 #Facebook Messenger ID for user so we know where to send response back to
                 recipient_id = message['sender']['id']
                 if message['message'].get('text'):
-                    response_sent_text = get_message()
+                    # response_sent_text = get_message()
                     response_sent_text = message['message']
                     send_message(recipient_id, response_sent_text)
                 #if user sends us a GIF, photo,video, or any other non-text item
